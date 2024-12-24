49aa773cc75af47e9ac90613a08150623d8e1c60
Guyven <guyven@pm.me>
2024-12-24 12:12:38 -0500

    porting cli-based plugin
    deno used for building

--- mod.ts
+++ mod.ts
@@ -1,30 +1,27 @@
-import * as path from "@std/path"
-import Kia from "@fathym/kia"
 import type { PluginOption } from "vite"
 
-const pluginName = "vite-plugin-test1"
+import ora from "ora"
+import {
+	debug,
+	error,
+	fsExecute,
+	fsExists,
+	info,
+	Defaults as util_defaults,
+} from "./util.ts"
+import { createReadStream } from "node:fs"
+import { join as pathJoin, basename } from "node:path"
+import { mkdir, readFile } from "node:fs/promises"
 
-const _wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-const error = (text: string, { extra = "" } = {}) =>
-	console.error(
-		`${pluginName} %cERROR:%c ${text} %c${extra}`,
-		"font-weight: bold; color: red",
-		"font-weight: reset; color: reset",
-		"color: grey",
-	)
+export const pluginName = "vite-plugin-tree-sitter"
 
 async function identifyCLI() {
 	for (const tsPath of [
-		path.join("node_modules", ".bin", "tree-sitter"),
+		pathJoin("node_modules", ".bin", "tree-sitter"),
 		"tree-sitter",
 	]) {
-		try {
-			const cmd = new Deno.Command(tsPath, { args: ["--version"] })
-			const out = await cmd.output()
-			if (out.success) return tsPath
-		} catch (_e) {
-			// console.error({ _e })
-		}
+		const cmdExe = await fsExecute(tsPath, { args: ["--version"] })
+		if (cmdExe.success) return tsPath
 	}
 	error("Unable to find 'tree-sitter' cli", {
 		extra:
@@ -38,6 +35,7 @@ export default function (
 		alwaysRebuild: boolean
 		wasmCacheDir: string
 		emBuildCacheDir: string
+		logLevel: "DEBUG" | "INFO" | "ERROR"
 	}>,
 ): PluginOption {
 	// console.log({ cwd: Deno.cwd(), parsers, options })
@@ -49,33 +47,51 @@ export default function (
 	if (!options) options = {}
 	const wasmCacheDir = options.wasmCacheDir || ".grammar"
 
-	console.log("test2")
+	if (options.logLevel == "DEBUG") {
+		util_defaults.showDebug = true
+		util_defaults.showInfo = true
+	} else if (options.logLevel == "ERROR") {
+		util_defaults.showDebug = false
+		util_defaults.showInfo = false
+	}
+
+	const wasmServeList = new Map()
+	wasmServeList.set(
+		"/tree-sitter.wasm",
+		pathJoin("node_modules", "web-tree-sitter", "tree-sitter.wasm"),
+	)
+
+	let runMode: "DEV" | "PROD" | null = null
+
 	return {
 		name: pluginName,
 		version: "0.2.0",
+		apply: ({ mode }) => {
+			runMode =
+				mode == "development" ? "DEV" : mode == "production" ? "PROD" : null
+			return true
+		},
 		async buildStart(_inputOptions: unknown) {
+			// console.log(_inputOptions)
+
 			const ts = await identifyCLI()
 			if (!ts) return
 
 			const grammars = new Map()
 			for (const parser of parsers) {
 				const err = (t: string) => error(t, { extra: parser })
-				const grammar_path = path.join(parser, "tree-sitter.json")
-				try {
-					const stat = await Deno.stat(grammar_path)
-					if (stat.isFile) {
-						try {
-							const parser_name = JSON.parse(
-								await Deno.readTextFile(grammar_path),
-							).grammars[0].name
-							grammars.set(parser_name, parser)
-						} catch (_e) {
-							// console.error({ _e })
-							err("unable to parse the grammar definition")
-						}
+				const grammar_path = pathJoin(parser, "tree-sitter.json")
+				const grammarExists = await fsExists(grammar_path)
+				if (grammarExists) {
+					try {
+						const parser_name = JSON.parse(await readFile(grammar_path, "utf8"))
+							.grammars[0].name
+						grammars.set(parser_name, parser)
+					} catch (_e) {
+						// console.error({ _e })
+						err("unable to parse the grammar definition")
 					}
-				} catch (_e) {
-					// console.error({ _e })
+				} else {
 					err("Unable to find 'tree-sitter.json'")
 				}
 			}
@@ -83,7 +99,7 @@ export default function (
 			// console.log({ grammars })
 
 			try {
-				await Deno.mkdir(wasmCacheDir, { recursive: true })
+				await mkdir(wasmCacheDir, { recursive: true })
 			} catch (_e) {
 				// already exists, but that's fine
 				error(JSON.stringify(_e))
@@ -93,50 +109,65 @@ export default function (
 
 			for (const [gName, gPath] of grammars) {
 				const outWasm = `tree-sitter-${gName}.wasm`
-				const outWasmPath = path.join(wasmCacheDir, outWasm)
+				const outWasmPath = pathJoin(wasmCacheDir, outWasm)
+				wasmServeList.set("/" + outWasm, outWasmPath)
 
-				const k = new Kia({
-					color: "blue",
+				const spinner = ora({
 					indent: 2,
+					color: "blue",
 					text: `${pluginName} : ${gName} : Building`,
 				})
 
 				if (!options.alwaysRebuild) {
-					try {
-						const stat = await Deno.stat(outWasmPath)
-						if (stat.isFile) {
-							k.succeed(`${pluginName} : ${gName} : Cached`)
-							continue
-						}
-					} catch {
-						// didn't exist, so we rebuild
+					if (await fsExists(outWasmPath)) {
+						spinner.succeed(`${pluginName} : ${gName} : Cached`)
+						continue
 					}
 				}
 
-				k.start()
+				spinner.start()
 
-				const cmd = new Deno.Command(ts, {
+				const exeResult = await fsExecute(ts, {
 					args: ["build", "--wasm", "--output", outWasmPath, gPath],
 					env: options.wasmCacheDir
 						? { EM_CACHE: options.wasmCacheDir }
 						: undefined,
 				})
 
-				const { success, stderr } = await cmd.output()
-				const decoder = new TextDecoder()
+				const { success, stderr } = exeResult
 				if (success) {
-					k.succeed(`${pluginName} : ${gName} : Built`)
+					spinner.succeed(`${pluginName} : ${gName} : Built`)
 				} else {
-					k.fail(`${pluginName} : ${gName} : Build failed`)
-					console.error(decoder.decode(stderr))
+					spinner.fail(`${pluginName} : ${gName} : Build failed`)
+					error("Tool output", { extra: stderr })
+				}
+			}
+
+			if (runMode == "PROD") {
+				for (const [wname, wpath] of wasmServeList) {
+					const wasmContent = await readFile(wpath)
+					this.emitFile({
+						type: "asset",
+						fileName: basename(wname),
+						source: wasmContent,
+					})
 				}
 			}
 		},
+		configureServer(server) {
+			server.middlewares.use((req, res, next) => {
+				if (req.url) {
+					debug("query", { extra: req.url })
+					const wasm = wasmServeList.get(req.url)
+					if (wasm) {
+						info("wasm serve", { extra: wasm })
+						res.writeHead(200, { "Content-Type": "application/wasm" })
+						createReadStream(wasm).pipe(res)
+					} else {
+						next()
+					}
+				}
+			})
+		},
 	}
 }
-
-// in the cache there should be two folders
-// one for the emcc worker
-// and one for the output files
-// they shoudl be copied to the build
-// or built if they dont' exist
---
+++ util.ts
@@ -0,0 +1,69 @@
+import { stat } from "node:fs/promises"
+import { spawn } from "node:child_process"
+import { join as pathJoin } from "node:path"
+import { pluginName } from "./mod.ts"
+
+export const Defaults: {
+	logPrefix: string
+	showInfo: boolean
+	showDebug: boolean
+} = {
+	logPrefix: pluginName,
+	showInfo: true,
+	showDebug: false,
+}
+
+export const info = (text: string, { extra = "", forceShow = false } = {}) =>
+	(forceShow || Defaults.showInfo) &&
+	console.error(
+		` %c${Defaults.logPrefix} %cINFO:%c ${text} %c${extra}`,
+		"color: darkgrey",
+		"font-weight: bold; color: yellow",
+		"font-weight: reset; color: reset",
+		"color: grey",
+	)
+export const debug = (text: string, { extra = "", forceShow = false } = {}) =>
+	(forceShow || Defaults.showDebug) &&
+	console.debug(
+		` %c${Defaults.logPrefix} %cDEBUG:%c ${text} %c${extra}`,
+		"color: darkgrey",
+		"font-weight: bold; color: blue",
+		"font-weight: reset; color: reset",
+		"color: grey",
+	)
+export const error = (text: string, { extra = "" } = {}) =>
+	console.error(
+		` %c${Defaults.logPrefix} %cERROR:%c ${text} %c${extra}`,
+		"color: darkgrey",
+		"font-weight: bold; color: red",
+		"font-weight: reset; color: reset",
+		"color: grey",
+	)
+
+export const fsExecute = (
+	path: string,
+	options: Partial<{ args: string[]; env: Record<string, string> }> = {},
+): Promise<{ success: boolean; stdout: string; stderr: string }> => {
+	path = Array.isArray(path) ? pathJoin(...path) : path
+	const { args, env } = options
+	const cmd = spawn(path, args, { env })
+	const result = { success: false, stderr: "", stdout: "" }
+	cmd.stdout.on("data", data => (result.stdout = data))
+	cmd.stderr.on("data", data => (result.stderr = data))
+	return new Promise(resolve =>
+		cmd.on("close", code => {
+			result.success = code == 0
+			resolve(result)
+		}),
+	)
+}
+export const fsExists = async (path: string): Promise<boolean> => {
+	path = Array.isArray(path) ? pathJoin(...path) : path
+	try {
+		await stat(path)
+		return true
+	} catch (_e) {
+		// console.error({ _e })
+	}
+	return false
+}
