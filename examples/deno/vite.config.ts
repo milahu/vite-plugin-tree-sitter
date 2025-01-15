b8f3a1ac740d2d08a34c210f14247bcb0d7b3fab
Guyven <guyven@pm.me>
2025-01-15 12:05:24 -0500

    updated examples

--- examples/deno/vite.config.ts
+++ examples/deno/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite"
-// import pluginTreeSitter from "@guyven/vite-plugin-tree-sitter"
-import pluginTreeSitter from "../../mod.ts"
+import pluginTreeSitter from "@guyven/vite-plugin-tree-sitter"
+// import pluginTreeSitter from "../../mod.ts"
 
 export default defineConfig({
 	plugins: [
 		pluginTreeSitter(["../../../../github/tree-sitter-sqlite"], {
-			logLevel: "TRACE",
+			// logLevel: "TRACE",
 		}),
 	],
 })
